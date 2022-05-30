7b3d17f4dfe798be2eed7a9887e9a9ea90a462de
tuduan1902 <89310065+tuduan1902@users.noreply.github.com>
2022-05-30 19:05:26 +0700

    Update and rename server.php to readData.php

---
+++ Test2/readData.php
@@ -0,0 +1,17 @@
+<?php
+// send a JSON message to website 
+header('Content-Type: application/json');
+
+// ket noi database
+include("config.php");
+// doc du lieu tu database
+$sql = "select * from data where id >(select max(id) from data)-20";
+$result = mysqli_query($conn,$sql);
+// gui du lieu len website
+$data = array();
+foreach ($result as $row){
+    $data[]=$row;
+}
+mysqli_close($conn);
+echo json_encode($data);
+?>
